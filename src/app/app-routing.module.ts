24a58d74423c426a5a77e062c9eb628558f7e0cf
sandhyaganta <gantasandhyavitasoft@gmail.com>
2023-12-20 14:17:43 +0530

    router path connect to the admin and user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainHomepageComponent } from './main-homepage/main-homepage.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { UserRegComponent } from './user-reg/user-reg.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'main-homepage',pathMatch:'full'},
-  {path:'main-homepage',component:MainHomepageComponent}
+  {path:'main-homepage',component:MainHomepageComponent},
+  {path:'admin',component:AdminLoginComponent},
+  {path:'user',component:UserRegComponent}
+  
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainHomepageComponent } from './main-homepage/main-homepage.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { UserRegComponent } from './user-reg/user-reg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainHomepageComponent
+    MainHomepageComponent,
+    AdminLoginComponent,
+    UserRegComponent
   ],
   imports: [
     BrowserModule,
