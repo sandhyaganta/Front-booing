1138169a994dfd60ff2034087555a8cbb24ef6b8
sandhyaganta <gantasandhyavitasoft@gmail.com>
2023-12-20 17:07:57 +0530

    admin-login completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainHomepageComponent } from './main-homepage/main-homepage.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { UserRegComponent } from './user-reg/user-reg.component';
+import { AdminHomepageComponent } from './admin-homepage/admin-homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainHomepageComponent,
     AdminLoginComponent,
-    UserRegComponent
+    UserRegComponent,
+    AdminHomepageComponent
   ],
   imports: [
     BrowserModule,
